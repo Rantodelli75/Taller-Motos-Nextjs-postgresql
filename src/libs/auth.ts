f368da92fcc4aa8203542f33319f99819c9a2fea
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2024-06-17 22:04:42 -0400

    added auth.ts and route for nextauth.ts

---
+++ src/libs/auth.ts
@@ -0,0 +1,45 @@
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import {PrismaAdapter} from "@next-auth/prisma-adapter"
+import prisma from "./db"
+import { compare } from "bcrypt"
+export const AuthOptions:NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    session: "jwt",
+    pages: {
+        signIn: '/auth/login'
+      },
+    
+    providers: [
+        CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            email: { label: "Email", type: "text", placeholder: "email@email.com" },
+            password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            if (!credentials?.email || !credentials?.password) {
+              return null
+            }
+            const existingUser = await prisma.usuario.findUnique({
+              where: {email: credentials?.email}
+            })
+            if (!existingUser) {
+              return null;
+            }
+
+            const passwordMatch = await compare(credentials.password, existingUser.clave)
+            if (!passwordMatch) {
+              return null;
+            }
+            return {
+              id: existingUser.Id + "",
+              username: existingUser.nombre,
+              email: existingUser.email,
+              rol: existingUser.rol
+            }
+          }
+         
+        })
+      ]
+}
