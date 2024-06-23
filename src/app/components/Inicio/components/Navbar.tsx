ea0234646729f523c35cda52b3f19e11e7caafdb
edqcov <145941060+edqcov@users.noreply.github.com>
2024-06-23 16:52:24 -0400

    update

--- src/app/components/Inicio/components/Navbar.tsx
+++ src/app/components/Inicio/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BiSolidSun, BiSolidMoon, BiUserCircle } from "react-icons/bi";
 import { useRouter } from 'next/navigation';
 import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
+import { BsArrowBarRight } from "react-icons/bs";
 
 
 
@@ -33,36 +34,36 @@ return (
             <nav className="hidden md:block">
                 <ul className="flex items-center gap-8">
                 <li className="py-4">
-                <a
+                <button
                     onClick={() => router.push('')}
                     className=" text-lg font-medium  text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
                 >
                 INICIO
-                </a>
+                </button>
                 </li>
                 <li className="py-4">
-                    <a
-                        
+                    <button
+                        onClick={() => router.push('/components/Inicio')}
                         className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
                     >
                     SERVICIOS
-                    </a>
+                    </button>
                 </li>
                 <li className="py-4">
-                    <a
+                    <button
                         onClick={() => router.push('/components/Mimoto')}
                         className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
                     >
                     MI MOTO
-                    </a>
+                    </button>
                 </li>
                 <li className="py-4">
-                    <a
+                    <button
                         onClick={() => router.push('/components/Contacto')}
                         className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
                     >
                     CONTACTO
-                    </a>
+                    </button>
                 </li>
                 <li className="py-4">
                 <Popover placement="bottom" offset={20} showArrow>
@@ -72,9 +73,15 @@ return (
                         </a>
                     </PopoverTrigger>
                     <PopoverContent>
-                        <div className="px-1 py-2 text-slate-950 bg-gray-200 rounded-xl">
-                            <div className="text-small font-bold">Aquí va el nombre de usuario</div>
-                            <div className="text-tiny">aquí los datos de usuario</div>
+                        <div className="px-1 py-2 text-slate-950 bg-gray-200 rounded-xl w-full h-36">
+                            <div className="text-small font-bold p-2 text-gray-500">Rafael José Covarrubio</div>
+                            <div className="text-tiny p-2 text-gray-400 font-semibold">rafcovte@gmail.com</div>
+                            <div className="flex text-orange-600 hover:text-amber-700 p-2">
+                            <a  className="flex text-orange-600 hover:text-amber-700" href="../auth/login">
+                                <BsArrowBarRight size={25}/>
+                            </a>
+                            <a className="ml-3" href="../auth/login">Salir</a>
+                            </div>
                         </div>
                     </PopoverContent>
                 </Popover>
