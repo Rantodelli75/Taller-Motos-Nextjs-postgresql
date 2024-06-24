cd30f484a383fb855202490085224fb9a37bb07d
Rantodelli75 <rdellicarpini17@gmail.com>
2024-06-24 09:57:11 -0400

    bajando cambios

---
+++ src/app/api/auth/register/moto/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import db from '@/libs/db'
+
+export async function POST(request: { json: () => any }) {
+    const data = await request.json()
+
+
+    const userFound = await db.moto.findUnique({
+        where: {
+            placa: data.placa
+        }
+    })
+
+    if (userFound) {
+        return NextResponse.json({
+            message: "El numero de placa ya fue registrado"
+        },{
+            status: 400
+        })
+    }
+
+    const newMoto = await db.moto.create({
+        data: {
+            marca: data.marca,
+            placa: data.placa,
+            modelo: data.modelo,
+            kilometraje: data.kilometraje
+        }
+    })
+
+    return NextResponse.json(newMoto)
+}
