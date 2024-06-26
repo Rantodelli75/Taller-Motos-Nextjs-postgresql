ed58b929a90cbb043e42b68a6b2960191cb9ad45
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2024-06-26 17:36:51 -0400

    added logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-
+"use client"
 
 // Component import
 import Navbar from "./components/Inicio/components/Navbar";
 import Hero from "./components/Inicio/components/Hero";
-import { createClient } from "@/utils/supabase/server";
 
-const App = async () => {
-  const supabase = createClient()
-
-    const { data, error } = await supabase.auth.getUser()
-    if (error || !data?.user) {
-      console.log(error)
-      console.log(data?.user)
-    }
+const App = () => {
+  
   return (
     <div className="scroll-smooth bg-white text-black overflow-x-hidden">
       <Navbar />
