3382259ddf6a4c71eecb48c81fd6c8696733f025
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2024-06-26 15:00:40 -0400

    added form

--- src/app/components/Mimoto/page.tsx
+++ src/app/components/Mimoto/page.tsx
@@ -1,8 +1,3 @@
-"use client"
-
-import React, { useEffect, useState } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 // Component import
 import Navbar from "@/app/components/Inicio/components/Navbar";
@@ -10,35 +5,9 @@ import Mimoto from "./component/mimoto";
 /*import Mimoto from "@/app/components/Mimoto/component/mimoto"*/
 
 const App = () => {
-  // dark mode start
-const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-);
-const element = document.documentElement;
-
-    useEffect(() => {
-    if (theme === "light") {
-        element.classList.add("light");
-        localStorage.setItem("theme", "light");
-    } else {
-        element.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-    }
-}, [element.classList, theme]);
-  // dark mode end
-
-    React.useEffect(() => {
-    AOS.init({
-        offset: 100,
-        duration: 800,
-        easing: "ease-in-sine",
-        delay: 100,
-    });
-    AOS.refresh();
-}, []);
     return (
     <div className="scroll-smooth bg-white text-black overflow-x-hidden">
-        <Navbar />
+        <Navbar/>
         <Mimoto/>
     </div>
 );
