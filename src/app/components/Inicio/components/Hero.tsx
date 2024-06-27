b551f86b953861d3118df452ac120502fdb36044
Juan De Ponte <de.ponte.juan.jose@gmail.com>
2024-06-26 23:31:08 -0400

    pene

--- src/app/components/Inicio/components/Hero.tsx
+++ src/app/components/Inicio/components/Hero.tsx
@@ -3,58 +3,56 @@ import React, { useEffect, useState } from "react";
 import motobg from "@/app/elements1/motobg.png"
 import motobackground from "@/app/elements1/motobackground.png"
 import AOS from "aos";
+import Link from "next/link"
 /* eslint-disable @next/next/no-img-element */
 
 const Hero =  () => {
     const router = useRouter();
     return (
-    <div className="mt-1 mb-3">
-        <div className="container min-h-[620px] flex">
-        <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-            <div
-            data-aos="zoom-in"
-            data-aos-duration="1500"
-            data-aos-once="false"
-            className="order-1 sm:order-2"
-            >
-            <img
-                src={motobg.src}
-                alt=""
-                className="sm:scale-110 relative -z-10 max-h-[500px] drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]"
-            />
-        </div>
-            <div className="space-y-5 order-2 sm:order-1 mb-6 ml-8 sm:pr-32 ">
-                <h1
-                data-aos="fade-up"
-                data-aos-delay="600"
-                className="text-5xl lg:text-7xl font-semibold font-serif"
+        <div className="mt-1 mb-3">
+            <div className="container min-h-[620px] flex">
+            <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+                <div
+                data-aos="zoom-in"
+                data-aos-duration="1500"
+                data-aos-once="false"
+                className="order-1 sm:order-2"
                 >
-                FORZA
-                <h1 
-                data-aos="fade-up"
-                data-aos-delay="600"
-                className="text-5xl lg:text-7xl font-semibold text-orange-500 font-serif"
-                >MOTOR
-                </h1>CLUB
-                </h1>
-            <p data-aos="fade-up" data-aos-delay="1000">
-            Somos una página dedicada al manejo y gestión de motocicletas, repuestos y contamos con un centro de servicios para el cuidado de tu inversión. {" "}
-            </p>
-            <button
-                data-aos="fade-up"
-                data-aos-delay="1500"
-                onClick={() => {
-                AOS.refreshHard();
-                }}
-                className="rounded-xl bg-neutral-950 hover:bg-orange-600 transition duration-500 py-2 px-6 text-white font-semibold"
-            >
-                <a onClick={() => router.push('/components/Registro')}>Registrar Moto</a> 
-            </button>
+                <img
+                    src={motobg.src}
+                    alt=""
+                    className="sm:scale-110 relative -z-10 max-h-[500px] drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]"
+                />
+            </div>
+                <div className="space-y-5 order-2 sm:order-1 mb-6 ml-8 sm:pr-32 ">
+                    <h1
+                    data-aos="fade-up"
+                    data-aos-delay="600"
+                    className="text-5xl lg:text-7xl font-semibold font-serif"
+                    >
+                    FORZA
+                    <span 
+                    data-aos="fade-up"
+                    data-aos-delay="600"
+                    className="text-5xl lg:text-7xl font-semibold text-orange-500 font-serif"
+                    >MOTOR
+                    </span>CLUB
+                    </h1>
+                <p data-aos="fade-up" data-aos-delay="1000">
+                Somos una página dedicada al manejo y gestión de motocicletas, repuestos y contamos con un centro de servicios para el cuidado de tu inversión. {" "}
+                </p>
+                <button
+                    data-aos="fade-up"
+                    data-aos-delay="1500"
+                    className="rounded-xl bg-neutral-950 hover:bg-orange-600 transition duration-500 py-2 px-6 text-white font-semibold"
+                >
+                    <Link href="/components/Registro">Registrar Moto</Link> 
+                </button>
+            </div>
+            </div>
         </div>
         </div>
-    </div>
-    </div>
-);
+    );
 };
 
 export default Hero;
--- src/app/components/Inicio/components/Navbar.tsx
+++ src/app/components/Inicio/components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiSolidSun, BiSolidMoon, BiUserCircle } from "react-icons/bi";
 import {Popover, PopoverTrigger, PopoverContent, Button} from "@nextui-org/react";
 import { FaSignOutAlt } from "react-icons/fa";
@@ -8,21 +8,36 @@ import { createClient } from "@/utils/supabase/client";
 import { logout } from "@/app/api/auth/logout/route";
 import db from "@/libs/db"
 import axios from "axios";
-
+import Link from "next/link"
 let showMenu = false
-
-const Navbar = async () => {
+let data = null
+const Navbar = () => {
+    const [user, setUser] = useState({})
     const supabase = createClient()
+    useEffect(() => {
+        const fetchUser = async () => {
+          try {
+            const { data, error } = await supabase.auth.getUser();
+            if (error || !data?.user) {
+              console.log(error)
+              console.log(data?.user)
+            }
+            if (error) {
+              console.error('Error fetching user:', error);
+            } else {
+              setUser(data.user);
+            }
+          } catch (error) {
+            console.error('Error fetching user:', error); // Handle potential errors
+          }
+        };
+    
+        fetchUser();
+      }, [supabase.auth]);
     
     
-    const { data, error } = await supabase.auth.getUser()
 
-    if (error || !data?.user) {
-      console.log(error)
-      console.log(data?.user)
-    }
-
-      const toggleMenu = () => {
+    const toggleMenu = () => {
         showMenu = !showMenu;
       };
 
@@ -33,102 +48,111 @@ const Navbar = async () => {
 
   //const isAuthenticated = Boolean(currentUser);
   
-        const isLoggedIn = Boolean(data?.user);
+        const isLoggedIn = Boolean(user != null);
 
-return (
-    <div
-        className=" z-10 shadow-lg w-full dark:text-white duration-300 bg-white"
-    >
-    <div className="container py-2 md:py-0">
-        <div className="flex justify-between items-center">
-            <div>
-            <span className="text-xl ml-5 font-bold text-gray-700 font-serif">FORZA<span className="text-xl font-bold text-orange-500 font-serif">MOTOR</span>CLUB</span>
+        return (
+            <div
+              className="z-10 shadow-lg w-full dark:text-white duration-300 bg-white"
+            >
+              <div className="container py-2 md:py-0">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <span className="text-xl ml-5 font-bold text-gray-700 font-serif">
+                      FORZA
+                      <span className="text-xl font-bold text-orange-500 font-serif">
+                        MOTOR
+                      </span>
+                      CLUB
+                    </span>
+                  </div>
+                  <nav className="hidden md:block">
+                    <ul className="flex items-center gap-8">
+                      <li className="py-4">
+                        <Link href="/">
+                          <button
+                            className="text-lg font-medium  text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
+                          >
+                            INICIO
+                          </button>
+                        </Link>
+                      </li>
+                      <li className="py-4">
+                        <Link href="/components/Servicios">
+                          <button
+                            className="text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
+                          >
+                            SERVICIOS
+                          </button>
+                        </Link>
+                      </li>
+                      <li className="py-4">
+                        <Link href="/components/Mimoto">
+                          <button
+                            className="text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
+                          >
+                            REGISTRAR MOTO
+                          </button>
+                        </Link>
+                      </li>
+                      <li className="py-4">
+                        <Link href="/components/Contacto">
+                          <button
+                            className="text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
+                          >
+                            CONTACTO
+                          </button>
+                        </Link>
+                      </li>
+                      <li className="py-4">
+                        <Link href="/components/Users">
+                          <button
+                            className="text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
+                          >
+                            GESTIONAR
+                          </button>
+                        </Link>
+                      </li>
+                    
+                      <li className="py-4">
+                        {!isLoggedIn? (
+                          <Link href="/auth/login">
+                            <button>Iniciar Sesion</button>
+                          </Link>
+                        ) : (
+                          <Popover placement="bottom" offset={20} showArrow>
+                            <PopoverTrigger>
+                              <button className="text-orange-600 hover:text-amber-700">
+                                <BiUserCircle size={25} />
+                              </button>
+                            </PopoverTrigger>
+                            <PopoverContent>
+                              <div className="px-1 py-2 text-slate-950 bg-gray-200 rounded-xl w-full h-36">
+                                <div className="text-small font-bold p-2 text-gray-500">
+                                  Bienvenido
+                                </div>
+                                <div className="text-tiny p-2 text-gray-400 font-semibold">
+                                </div>
+                                <div className="flex text-orange-600 hover:text-amber-700 p-2">
+                                  <Link href="../auth/login">
+                                    <button className="flex text-orange-600 hover:text-amber-700">
+                                      <FaSignOutAlt size={25} />
+                                    </button>
+                                  </Link>
+                                  <button onClick={logout} className="ml-3">
+                                    Salir
+                                  </button>
+                                </div>
+                              </div>
+                            </PopoverContent>
+                          </Popover>
+                        )}
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
             </div>
-            <nav className="hidden md:block">
-                <ul className="flex items-center gap-8">
-                <li className="py-4">
-                <button
-                    className=" text-lg font-medium  text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
-                ><a href="/">
-
-                INICIO
-                </a>
-                </button>
-                </li>
-                <li className="py-4">
-                    <button
-                        className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
-                    >
-                        <a href="/components/Servicios">
-
-                    SERVICIOS
-                        </a>
-                    </button>
-                </li>
-                <li className="py-4">
-                    <button
-                        className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
-                    ><a href="/components/Mimoto">
-
-                    REGISTRAR MOTO
-                    </a>
-                    </button>
-                </li> 
-                <li className="py-4">
-                    <button
-                        className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
-                    >
-                        <a href="/components/Contacto">
-
-                    CONTACTO
-                        </a>
-                    </button>
-                </li>
-                <li className="py-4">
-                    <button
-                        onClick={() => redirect('/components/Users')}
-                        className=" text-lg font-medium text-gray-600 hover:text-primary py-2 hover:border-b-2 hover:border-orange-600 transition-colors duration-500  "
-                    >
-                        <a href="/components/USers">
-                        
-                    GESTIONAR
-                        </a>
-                    </button>
-                </li> 
-
-                
-                
-                <li className="py-4">
-                { !isLoggedIn ? (<a href="/auth/login">Iniciar Sesion</a>) : (<Popover placement="bottom" offset={20} showArrow>
-                    <PopoverTrigger>
-                        <a  className="text-orange-600 hover:text-amber-700">
-                        <BiUserCircle size={25}/>
-                        </a>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                        <div className="px-1 py-2 text-slate-950 bg-gray-200 rounded-xl w-full h-36">
-                            <div className="text-small font-bold p-2 text-gray-500">Bienvenido</div>
-                            <div className="text-tiny p-2 text-gray-400 font-semibold">{data?.user?.email}</div>
-                            <div className="flex text-orange-600 hover:text-amber-700 p-2">
-                            <a  className="flex text-orange-600 hover:text-amber-700" href="../auth/login">
-                                <FaSignOutAlt size={25}/>
-                            </a>
-                            <button >
-
-                            <a onClick={() => logout()} className="ml-3">Salir</a>
-                            </button>
-                            </div>
-                        </div>
-                    </PopoverContent>
-                </Popover>)}
-                </li>
-
-                </ul>
-            </nav>
-        </div>
-    </div>
-    </div>
-);
-};
+          );
+        };
 
 export default Navbar;
